757acc9476cc78ba0cd91d14eea271452e5ce193
vladislav.fedotov <vladislav.fedotov@nordigy.ru>
2015-03-01 21:11:45 +0300

    Logging was added to UserMealRest.

--- src/main/java/ru/javawebinar/topjava/web/UserMealRest.java
+++ src/main/java/ru/javawebinar/topjava/web/UserMealRest.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.web;
 
+import ru.javawebinar.topjava.LoggerWrapper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +12,14 @@ import java.io.IOException;
  * Created by vladislav.fedotov on 01.03.2015.
  */
 public class UserMealRest extends HttpServlet {
+    private static final LoggerWrapper LOG = LoggerWrapper.get(UserMealRest.class);
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("GET request received");
         request.getRequestDispatcher("/userMealRest.jsp").forward(request, response);
     }
 }
